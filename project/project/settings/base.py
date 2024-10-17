7721e72bad86047e87498706ad1d9886302ff8b1
MohamedHamed12 <mh1779371@gmail.com>
2024-10-17 14:37:43 +0300

    remove secrets

--- project/project/settings/base.py
+++ project/project/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the django-insecure-5^31#!z1k!=(+*w7g+&qjw#in1sb7!(2bc7bsf(7kciy0_35@ysecret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
 
